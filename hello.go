04934af9a920ac76f17407234639fc95df156b3a
Elise McCrorie <elise.mccrorie@gmail.com>
2022-05-19 15:35:58 -0700

    hello world

--- hello.go
+++ hello.go
@@ -2,8 +2,13 @@ package main
 
 import "fmt"
 
+const enPrefix = "Hello, "
+
 func Hello(name string) string {
-	return "Hello, " + name
+	if name == "" {
+		name = "World"
+	}
+	return enPrefix + name
 }
 
 func main() {
