f1c10f55fb5816e84eb05c3e648001e9185c5d82
Elise McCrorie <elise.mccrorie@gmail.com>
2022-05-31 13:33:28 -0700

    SumAll arrays and tests

--- arrays/sum.go
+++ arrays/sum.go
@@ -7,3 +7,27 @@ func Sum(numbers []int) int {
 	}
 	return sum
 }
+
+func SumAll(numbersToSum ...[]int) []int {
+	var sums []int
+
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+	return sums;
+}
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+
+	for _, numbers := range numbersToSum {
+		// check that slice is not empty
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			// sum the numbers in the slice, excluding the first number
+			sums = append(sums, Sum(numbers[1:]))
+		}
+	}
+	return sums;
+}
