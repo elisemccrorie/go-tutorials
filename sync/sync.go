bc891f8eb1a8bea07c5255c332b82336399ee5bd
Elise McCrorie <elise.mccrorie@gmail.com>
2023-05-30 10:01:51 -0700

    context, sync, and intro to property based tests tutorials

---
+++ sync/sync.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"sync"
+)
+
+func NewCounter() *Counter {
+	return &Counter{}
+}
+
+type Counter struct {
+	mu sync.Mutex
+	count int
+}
+
+func (c *Counter) Inc() {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.count++
+}
+
+func (c *Counter) Value() int {
+	return c.count
+}
